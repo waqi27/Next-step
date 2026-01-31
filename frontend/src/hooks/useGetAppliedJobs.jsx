c0abaea07c870c8bde7e1360ca783a0ab767e919
Waqi khan <kwaqi18@gmail.com>
2026-01-31 18:14:55 +0530

    Add files via upload

---
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -0,0 +1,24 @@
+import { setAllAppliedJobs } from '@/redux/applicationSlice';
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+const useGetAppliedJobs = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchAppliedJobs = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get('https://nextstep-rqja.onrender.com/api/v1/application/get');
+                if (res.data.success) {
+                    dispatch(setAllAppliedJobs(res.data.application))
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJobs();
+    }, [])
+}
+
+export default useGetAppliedJobs
